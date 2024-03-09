452d505e7c1b756d01186f1f271567c50a396de4
OceusGreycastle <ttvprojectmisfit@gmail.com>
2024-03-09 17:26:02 -0600

    FIRST TRY!?

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -3,12 +3,21 @@
 #include "lists.h"
 
 /**
- * 
- * 
- * 
+ * list_len -
+ * @h:
+ *
+ * Return -
 */
 
 size_t list_len(const list_t *h)
 {
-    
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+			h = h->next;
+			count++;
+	}
+
+	return (count);
 }
