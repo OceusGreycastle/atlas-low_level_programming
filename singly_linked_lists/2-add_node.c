d23c50451da67e92ac2af11065c82ea1d9af1b2c
OceusGreycastle <ttvprojectmisfit@gmail.com>
2024-03-09 20:03:48 -0600

    quick save

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,5 +10,21 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+    if (head == NULL)
+    {
+        return (NULL);
+    }
+
+    list_t *nugget = NULL;
+
+    nugget = malloc(sizeof(list_t));
+
+    if (nugget == NULL)
+    {
+        return (NULL);
+    }
     
+
+
+
 }
