798a38ec6d80829ed433dc4c5e8af7fbb04ac625
OceusGreycastle <ttvprojectmisfit@gmail.com>
2024-03-11 00:48:09 -0500

    BETTY

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -8,48 +8,47 @@
  * add_node_end - adds a node to the end of a list.
  * @head: pointer to the head of list
  * @str: string to be copied
- * 
+ *
  * Return: Always
 */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *nugget;
-    list_t *temp_nugget;
+	list_t *nugget;
+	list_t *temp_nugget;
 
-    nugget = NULL;
+	nugget = NULL;
 
-    nugget = malloc(sizeof(list_t));
+	nugget = malloc(sizeof(list_t));
 
-    if (head == NULL)
-    {
-        return (NULL);
-    }
-    if (nugget == NULL)
-    {
-        return (NULL);
-    }
-    
-    nugget->str = strdup(str);
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	if (nugget == NULL)
+	{
+		return (NULL);
+	}
 
-    nugget->len = _strlen(str);
+	nugget->str = strdup(str);
 
-    nugget->next = NULL;
+	nugget->len = _strlen(str);
 
-    if (*head == NULL)
-    {
-        *head = nugget;
-    }
-    else
-    {
-        temp_nugget = *head;
+	nugget->next = NULL;
 
-        while (temp_nugget->next != NULL)
-        {
-            temp_nugget= temp_nugget->next;
-        }
-        temp_nugget->next = nugget;
-    }
-    return (nugget);
+	if (*head == NULL)
+	{
+		*head = nugget;
+	}
+	else
+	{
+		temp_nugget = *head;
 
+		while (temp_nugget->next != NULL)
+		{
+			temp_nugget= temp_nugget->next;
+		}
+		temp_nugget->next = nugget;
+	}
+	return (nugget);
 }
