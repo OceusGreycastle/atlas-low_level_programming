64a7dee2e57215a227fdad2c481d480778e97e3b
OceusGreycastle <ttvprojectmisfit@gmail.com>
2024-03-10 22:01:30 -0500

    docunentation prepped and finalized

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,9 +3,11 @@
 #include "lists.h"
 
 /**
+ * add_node_end - adds a node to the end of a list.
+ * @head: head of list
+ * @str: string to be copied
  * 
- * 
- * 
+ * Return: Always
 */
 
 list_t *add_node_end(list_t **head, const char *str)
