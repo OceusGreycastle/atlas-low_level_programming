dab246e5afc1428b5ef4d54790f501d74168195d
OceusGreycastle <ttvprojectmisfit@gmail.com>
2024-03-12 08:08:02 -0500

    betty checks

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -3,21 +3,22 @@
 #include "lists.h"
 
 /**
- * 
- * 
- * 
+ * print_listint - prints the elements of a list
+ * @h: Name of size_t list
+ *
+ * Return: Number of Nodes
 */
 
 size_t print_listint(const listint_t *h)
 {
-    
-    size_t count = 0;
 
-    while (h != NULL)
-    {
-            printf("%i\n", h->n);
-            h = h->next;
-            count++;   
-    }
-    return (count);
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+			printf("%i\n", h->n);
+			h = h->next;
+			count++;
+	}
+	return (count);
 }
