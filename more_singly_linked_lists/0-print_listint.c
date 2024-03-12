d98917388697a348071ec32297f83d50ad6b8006
OceusGreycastle <ttvprojectmisfit@gmail.com>
2024-03-12 08:05:26 -0500

    point to n

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -15,7 +15,7 @@ size_t print_listint(const listint_t *h)
 
     while (h != NULL)
     {
-            printf("%i", h);
+            printf("%i", h->n);
             h = h->next;
             count++;   
     }
