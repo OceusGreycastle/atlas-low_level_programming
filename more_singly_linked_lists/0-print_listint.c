2db7543c85654a74422043ab84b0cc2ef8691cfd
OceusGreycastle <ttvprojectmisfit@gmail.com>
2024-03-12 08:11:22 -0500

    spacing

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -15,9 +15,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-			printf("%i\n", h->n);
-			h = h->next;
-			count++;
+		printf("%i\n", h->n);
+		h = h->next;
+		count++;
 	}
 	return (count);
 }
