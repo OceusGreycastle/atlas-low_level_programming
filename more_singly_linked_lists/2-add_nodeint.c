00b9919400eb99a20031b424555bf56df1962c4a
OceusGreycastle <ttvprojectmisfit@gmail.com>
2024-03-12 08:22:22 -0500

    FIrsT TrY?

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -5,12 +5,35 @@
 #include "strlen.c"
 
 /**
- * 
- * 
- * 
+ *
+ *
+ *
 */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 
+	listint_t *nugget;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	nugget = NULL;
+
+	nugget = malloc(sizeof(listint_t));
+
+	if (nugget == NULL)
+	{
+		return (NULL);
+	}
+
+	nugget->n = strdup(n);
+
+	nugget->next = *head;
+
+	*head = nugget;
+
+	return (nugget);
 }
