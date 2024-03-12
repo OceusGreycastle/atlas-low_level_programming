350829967250a23157d15aaf64af3e5394524e64
OceusGreycastle <ttvprojectmisfit@gmail.com>
2024-03-12 08:16:15 -0500

    first try?

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -3,12 +3,21 @@
 #include "lists.h"
 
 /**
- * 
- * 
- * 
+ * listint_len - returns number of elements counted in a list
+ * @h: pointer to a list size_t
+ *
+ * Return: number of elements
 */
 
 size_t listint_len(const listint_t *h)
 {
-    
+   	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
 }
