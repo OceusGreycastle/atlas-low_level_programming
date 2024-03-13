99faa63bbf06d93c6f0d48a07e78cf90a0ba1712
OceusGreycastle <ttvprojectmisfit@gmail.com>
2024-03-13 10:45:18 -0500

    again?

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -3,12 +3,21 @@
 #include "lists.h"
 
 /**
- * 
- * 
- * 
+ * dlistint_len - returns number of elements counted in a list
+ * @h: name of a size_t list
+ *
+ * Return: Number of elements
 */
 
 size_t dlistint_len(const dlistint_t *h)
 {
-    
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
 }
