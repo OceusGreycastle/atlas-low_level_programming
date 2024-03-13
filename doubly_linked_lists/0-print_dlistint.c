a28c88eba69c541f8c07e08007956d9148eebb0a
OceusGreycastle <ttvprojectmisfit@gmail.com>
2024-03-13 10:42:33 -0500

    I'm literally crying

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,14 +3,15 @@
 #include "lists.h"
 
 /**
- * 
- * 
- * 
+ * print_dlistint - prints the elements of a list doubly linked
+ * @h: name of size_t list
+ *
+ * Return: Number of nodes
 */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
