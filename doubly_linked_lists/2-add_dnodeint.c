80a2c77cd363ba945db4e0daa8d887f2f8fb97b5
OceusGreycastle <ttvprojectmisfit@gmail.com>
2024-03-13 10:48:09 -0500

    I'm literally so done, this is making me brain ded

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -3,12 +3,36 @@
 #include "lists.h"
 
 /**
+ * add_dnodeint - adds a node to the head of a list
+ * @head: head of list
+ * @n: node to be inserted
  * 
- * 
- * 
+ * Return: number of nodes
 */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    
+    dlistint_t *nugget;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	nugget = NULL;
+
+	nugget = malloc(sizeof(dlistint_t));
+
+	if (nugget == NULL)
+	{
+		return (NULL);
+	}
+
+	nugget->n = n;
+
+	nugget->next = *head;
+
+	*head = nugget;
+
+	return (nugget);
 }
